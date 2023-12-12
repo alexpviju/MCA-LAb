d42efef72bd05da8085348f4917dce86eff44de2
alexpviju <150989379+alexpviju@users.noreply.github.com>
2023-12-12 08:58:36 +0000

    Update arrayfind.c

--- s1/ADS/arrayfind.c
+++ s1/ADS/arrayfind.c
@@ -1,30 +1,25 @@
 #include<stdio.h>
-void main()
-{
- int a[20],small=0,i,size,lar=0;
- printf(" enter the size of array a :: ");
- scanf("%d",&size); 
- if(size>20)
- printf("invalid size");
- else
- {
-  printf("enter the array elements  :: ");
-  for(i=0;i<size;i++)
-  {
-   scanf("%d",&a[i]);
-  }
-  for(i=0;i<size;i++)
-   {
-    if(a[i]>a[i-1])
-    {
-     lar=a[i];
-    }
-    if(a[i]<a[i-1])
-    {
-     small=a[i];
-    }
- 
- }printf("\n LARGEST is %d ",lar);
- printf("\n SMALLEST is %d",small);
-}
+
+void main(){
+     int lar,sam,a[10],i,size;
+     printf("Enter size of array: ");
+     scanf("%d",&size);
+     printf("Enter array element: ");
+     for(i=0;i<size;i++){
+          printf("\nArray[%d] ",i+1);
+          scanf("%d",&a[i]);
+     }
+     lar=sam=a[0];
+     for(i=0;i<size;i++){
+          if(lar>a[i]){
+               lar=a[i];
+          }
+          if(sam<a[i]){
+               sam=a[i];
+          }
+     }
+     
+     
+     printf("\nLargest element : %d",lar);
+     printf("\nSmallest element : %d\n",sam);
 }
