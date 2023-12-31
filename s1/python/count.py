4c5aff6ba39e3307a757109ebc364accb3fcb366
alexpviju <150989379+alexpviju@users.noreply.github.com>
2023-12-31 23:01:16 +0530

    Delete s1/python/count.py

--- s1/python/count.py
+++
@@ -1,8 +0,0 @@
-word=input("enter the Word ::  ")
-count={}
-for i in word:
-    if i in count:
-        count[i]=count[i]+1
-    else:
-        count[i]=1
-print ("count of letters in sting ::  \n",str(count))
